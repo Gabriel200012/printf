5e1af20515290889d9c981d82240e67f867d78d2
Mavipop <70797502+HaywhyD@users.noreply.github.com>
2023-04-25 22:34:37 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -4,7 +4,7 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
+ * @c: Char to be evaluated
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -25,7 +25,7 @@ return (0);
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
- * @i: Index at which to start appending.
+ * @i: Index at which to start appending
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */

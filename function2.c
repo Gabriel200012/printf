268284c201c13df133fa9b05dce65cd58ea2e5ef
Mavipop <70797502+HaywhyD@users.noreply.github.com>
2023-04-25 22:29:24 +0100

    Update function2

--- function2.c
+++ function2.c
@@ -8,7 +8,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed.
+ * Return: Number of chars printed
  */
 int print_pointer(va_list types, char buffer[],
 int flags, int width, int precision, int size)
